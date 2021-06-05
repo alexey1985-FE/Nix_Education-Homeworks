d71c2cb3333a660a869faba4460a56c3398981a6
Alexey Yevkov <71827775+alexey1985-FE@users.noreply.github.com>
2021-06-05 13:13:08 +0300

    Homework_9_JS

--- Yevkov--Nix-homework-9-JS/main.js
+++ Yevkov--Nix-homework-9-JS/main.js
@@ -136,7 +136,7 @@
 
 // function truncate(str, maxlength) {
 // 	if (str.length > maxlength) {
-// 		return str.slice(0, maxlength - 1) + '...';
+// 		return str.slice(0, maxlength - 1) + '…';
 // 	}
 // 	return str;
 // }
