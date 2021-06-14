7cdb1222bb307a1484828a6732c537934bffe3a3
Alexey Yevkov <71827775+alexey1985-FE@users.noreply.github.com>
2021-06-14 16:09:02 +0300

    Delete Yevkov--Nix-homework-12-JS  directory

--- Yevkov--Nix-homework-12-JS /Задание №4/main.js
+++
@@ -1,9 +0,0 @@
-// Задание No4. Скрыть элемент по нажатию кнопки
-// Добавьте JavaScript к кнопке button, чтобы при нажатии элемент <div id="text"> исчезал.
-
-let buttonHide = document.querySelector('#hider');
-let textHide = document.querySelector('#text');
-
-buttonHide.addEventListener('click', () => {
-	textHide.style.display = 'none';
-});
--- Yevkov--Nix-homework-12-JS /Задание №7/main.js
+++
@@ -1,9 +0,0 @@
-// Задание No7. Добавить кнопку закрытия
-// Есть список сообщений.
-// При помощи JavaScript для каждого сообщения добавьте в верхний правый угол кнопку
-// закрытия.
-
-document.querySelectorAll('.pane').forEach(item => {
-	item.insertAdjacentHTML('afterbegin', '<button class="remove-button">[x]</button>');
-	item.firstChild.addEventListener('click', () => item.remove());
-});
